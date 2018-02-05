7580bd587b377827b4b4fe0ff8703f245fc8323f
PaulDunn <PaulDunn@armsign-sm-lt.armsign.local>
2018-02-05 11:55:06 +1100

    Spooling up an audio recorder

--- index.php
+++ index.php
@@ -11,14 +11,35 @@ and open the template in the editor.
         
         <!-- CSS -->
         <link href="CSS/animate.min.css" rel="stylesheet" type="text/css"/>
+        <link href="CSS/PLTravers.css" rel="stylesheet" type="text/css"/>
         
         <!-- Javascript -->
         <script src="Scripts/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="Scripts/recorder.js" type="text/javascript"></script>
+        <script src="Scripts/wavesurfer.min.js" type="text/javascript"></script>
     </head>
     <body>
-        <?php echo "You put your code in here!";  ?>
         
-        <p>This ought to display the required bits and pieces for the PL Travers Building</p>
+        <div class="animated bounceInDown">
+
+            <h1>Welcome to the Story Bank!</h1>
+        
+            <p>Listen to our stories</p>
+            
+            
+        <!--    This section normally displays it's stuff based on the url parameters, am I right? 
+                I guess I could have it do something more complex, but I'll want the sections broken up correctly. -->
+        
+        <?php echo "You put your code in here!";  ?>
+            
+            
+        
+        <aside>This ought to display the required bits and pieces for the PL Travers Building</aside>
+        
+
+        
+        
+        </div>
         
         
     </body>
